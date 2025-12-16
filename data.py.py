867192c3d053d172462a2ff81bf7132287bcba44
ramm-2005 <rammreddyy18@gmail.com>
2025-12-16 14:27:22 +0530

    Add files via upload

---
+++ data.py.py
@@ -0,0 +1,28 @@
+
+age = 20
+
+
+price = 99.5
+
+
+name = "Alice"
+
+
+is_student = True
+
+result = None
+
+subjects = ["Math", "Science", "English"]
+
+person = {
+    "name": "Alice",
+    "age": 20
+}
+
+print(type(age))
+print(type(price))
+print(type(name))
+print(type(is_student))
+print(type(result))
+print(type(subjects))
+print(type(person))
